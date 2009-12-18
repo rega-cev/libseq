989789f9e49a00d3199cabe32e8355ffca8c151e
Gertjan Beheydt <gertjan.beheydt+git@gmail.com>
2009-12-18 13:32:13 +0100

    ambiguous nucleotides translate to one AA if possible

---
+++ test/src/NeedlemanWunsh.C
@@ -0,0 +1,22 @@
+#include "Align.h"
+
+#include <fstream>
+
+using namespace seq;
+
+int main(int argc, char **argv)
+{
+  std::ifstream s1(argv[1]);
+  std::ifstream s2(argv[2]);
+
+  AASequence seq1, seq2;
+  s1 >> seq1;
+  s2 >> seq2;
+  std::cerr << seq1 << std::endl;
+  std::cerr << seq2 << std::endl;
+  double result = Align(seq1, seq2);
+  std::cerr << std::endl;
+  std::cerr << seq1 << std::endl;
+  std::cerr << seq2 << std::endl;
+  return 0;
+}
